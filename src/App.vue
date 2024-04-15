f0685431be9342a323a10f25cd40296e214488f6
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-04-15 13:14:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+    export default{
+        components: {
+        }
+    }
+</script>
+
+<template>
+    <h1 class="d-flex jst-cntr">ciao sono un titolo</h1>
+</template>
+
+<style  lang="scss">
+  @use './style/generic';
+
+</style>
